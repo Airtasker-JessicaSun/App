a47d7175606dd738affff81bc1dd67e220fdcfb8
Jessica J Sun <jessi.slm@hotmail.com>
2020-01-29 20:16:18 +1100

    Updates

---
+++ precache-manifest.cdedd48f4a276624cd87ee67f96c4d8d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f5529525978706553ff5f90b66922a4",
+    "url": "/App/index.html"
+  },
+  {
+    "revision": "9f6122b1e3bc6c7a85cd",
+    "url": "/App/static/css/main.6da63159.chunk.css"
+  },
+  {
+    "revision": "79cb71998ae0de3ae7a4",
+    "url": "/App/static/js/2.c6d6c3dc.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/App/static/js/2.c6d6c3dc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f6122b1e3bc6c7a85cd",
+    "url": "/App/static/js/main.bb148ddf.chunk.js"
+  },
+  {
+    "revision": "d4b8fd62cde18c456afb",
+    "url": "/App/static/js/runtime-main.aa9ad028.js"
+  },
+  {
+    "revision": "19217f852797c77d4f57d9d03b307c2f",
+    "url": "/App/static/media/6d532ccb-086c-4000-bab2-5d16862fcf01.19217f85.ttf"
+  },
+  {
+    "revision": "3406d17bc8c45dcabab13535672481b6",
+    "url": "/App/static/media/a29947fd-0c53-492b-b2f4-399b01153b30.3406d17b.eot"
+  },
+  {
+    "revision": "b68eb74d7cea5cca6894d23a598616ed",
+    "url": "/App/static/media/background.b68eb74d.jpg"
+  },
+  {
+    "revision": "e116b64c2f2f90b6bc4829a062802a50",
+    "url": "/App/static/media/d42dd843-62b0-4623-8855-d5882512c3c9.e116b64c.woff"
+  },
+  {
+    "revision": "f9a3c16cccece917af42c82563316c7a",
+    "url": "/App/static/media/d9896899-f08c-4750-a874-a9e10d83c2cb.f9a3c16c.woff2"
+  },
+  {
+    "revision": "26eae9eb8fce5ffee35d6b55d9707127",
+    "url": "/App/static/media/login.26eae9eb.jpg"
+  }
+]);
